41deffb874bfa1b729c9b3fbbe1bf66c6c758194
rish17 <rishabh.jain@livehealth.in>
2019-11-29 23:17:49 +0530

    Final changes made.

--- ticketing.py
+++ ticketing.py
@@ -1,21 +1,55 @@
-class Ticket:
+
+class Ticket():
     number_slots = 0
     parking_slot = 0
     parking_list = []
 
     def __init__(self,number_slot):
         self.number_slots = number_slot
+        print("Created a parking slot with " + str(self.number_slots) + " slots")
 
     def display_slots(self):
         print(self.number_slots, self.parking_slot, self.parking_list)
     
+
+    def get_empty_slot(self):
+        if(self.parking_slot < self.number_slots and self.number_slots != 0):
+            empty_list = [index for index,obj in enumerate(self.parking_list,start=1) if not bool(obj)]
+            val = self.parking_slot
+            if(val==0 or len(empty_list)<=0 or (len(empty_list)>0 and empty_list[0]>val)):
+                self.parking_slot += 1
+                return self.parking_slot
+            elif(len(empty_list)>0 and empty_list[0]<val):
+                return empty_list[0]
+        elif(self.parking_slot > self.number_slots and self.number_slots!=0):
+            empty_list = [index for index,obj in enumerate(self.parking_list,start=1) if not bool(obj)]
+            if(len(empty_list)>0):
+                return empty_list[0]
+            else:
+                return -1
+            
+    
     def register_details(self, number_plate, car_color):
-        reg_dict = {}
-        reg_dict['numberPlate'] = number_plate
-        reg_dict['carColor'] = car_color
-        self.parking_slot += 1
-        reg_dict['parkingSlot'] = self.parking_slot
-        self.parking_list.append(reg_dict)
+        if(self.parking_slot < self.number_slots or self.get_empty_slot()!=-1):
+            slot_val = self.get_empty_slot()
+            if(slot_val>self.number_slots or self.number_slots == -1):
+                print("Parking is full, Sorry")
+                return
+            elif(slot_val>=len(self.parking_list)):    
+                reg_dict = {}
+                reg_dict['numberPlate'] = number_plate
+                reg_dict['carColor'] = car_color
+                reg_dict['parkingSlot'] = slot_val
+                self.parking_list.append(reg_dict)
+            elif(slot_val<=len(self.parking_list)):
+                reg_dict = {}
+                reg_dict['numberPlate'] = number_plate
+                reg_dict['carColor'] = car_color
+                reg_dict['parkingSlot'] = slot_val
+                self.parking_list[slot_val-1] = reg_dict
+            print("Allocated slot number: "+ str(slot_val))
+        else:
+            print("Parking is full")
     
     def vacate_slot(self, car_slot):
         if(len(self.parking_list)>0):
@@ -62,26 +96,53 @@ class Ticket:
                     print("No slot number for car registration number as "+ data)
         else:
             print("No data as no one has parked their vehicles")
+            
+
 
     def exit_system(self):
+        print("Bye")
         quit()
 
-t = Ticket(6)
-t.get_slot_number("123",1)
-t.display_slots()
-t.register_details("123", "White")
-t.display_slots()
-t.register_details("124", "Black")
-t.display_slots()
-t.display_status()
-t.get_registration_numb("Black")
-# t.vacate_slot(1)
-t.display_slots()
-t.display_status()
-t.get_registration_numb("White")
-t.get_slot_number("Black",0)
-t.get_slot_number("123",1)
-t.exit_system()
+# t = Ticket(6)
+# t.register_details("123","White")
+# t.register_details("124","Red")
+# t.register_details("125","Black")
+# t.register_details("126","Black")
+# t.display_slots()
+# t.display_status()
+# t.vacate_slot(2)
+# t.display_slots()
+# t.display_status()
+# t.register_details("127","Blue")
+# t.display_slots()
+# t.display_status()
 
+    
 
+while(True):
+    command = raw_input("Welcome to the ticketing console. Hope it works fine for you.\n")
+    # t = Ticket(0)
+    if(command.split()[0] == 'create_parking_lot'):
+        park_slots_total = command.split()[1]
+        t = Ticket(int(park_slots_total))
+    if(command.split()[0] == 'park'):
+        reg_number = command.split()[1]
+        car_colour = command.split()[2]
+        t.register_details(reg_number,car_colour)
+    if(command.split()[0] == 'leave'):
+        slot_to_vacate = command.split()[1]
+        t.vacate_slot(int(slot_to_vacate))
+    if(command.split()[0] == 'status'):
+        t.display_status()
+    if(command.split()[0] == 'registration_numbers_for_cars_with_colour'):
+        colour_of_car = command.split()[1]
+        t.get_registration_numb(colour_of_car)
+    if(command.split()[0] == 'slot_numbers_for_cars_with_colour'):
+        colour_of_car = command.split()[1]
+        t.get_slot_number(colour_of_car,0)
+    if(command.split()[0] == 'slot_number_for_registration_number'):
+        registr_number = command.split()[1]
+        t.get_slot_number(registr_number,1)
+    if(command.split()[0] == 'exit'):
+        t.exit_system()
 
